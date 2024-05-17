7fb14bb43bd6e03bd29b9583a9ac1297d4c54322
Draburst <135053114+Draburst@users.noreply.github.com>
2024-05-17 21:05:25 +0300

    Add files via upload

---
+++ game.js
@@ -0,0 +1,21 @@
+let num1, num2, correctAnswer;
+
+function generateQuestion() {
+    num1 = Math.floor(Math.random() * 10) + 1;
+    num2 = Math.floor(Math.random() * 10) + 1;
+    correctAnswer = num1 + num2;
+    document.getElementById('question').innerText = `What is ${num1} + ${num2}?`;
+    document.getElementById('answer').value = '';
+    document.getElementById('feedback').innerText = '';
+}
+
+function checkAnswer() {
+    const userAnswer = parseInt(document.getElementById('answer').value);
+    if (userAnswer === correctAnswer) {
+        document.getElementById('feedback').innerText = 'Correct!';
+    } else {
+        document.getElementById('feedback').innerText = 'Incorrect. Try again!';
+    }
+}
+
+window.onload = generateQuestion;
